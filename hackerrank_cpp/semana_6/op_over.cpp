e44ee637c1c220f7cf7bfa3fabd8af0c3b7cea78
davi-Desktop <davi12564387@gmail.com>
2025-06-08 16:31:17 -0300

    semana_6

---
+++ hackerrank_cpp/semana_6/op_over.cpp
@@ -0,0 +1,64 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Matrix{
+    public:
+        vector<vector<int>> a;
+
+        Matrix() = default;
+        
+        Matrix(int m, int n) : a(m, vector<int>(n, 0)){}
+        
+        Matrix operator+(Matrix const& matriz) const{
+            int m = a.size();
+            int n = a[0].size();
+            Matrix resultado(m,n);
+            for(int i = 0; i < a.size(); i++){
+                for(int j = 0; j < a[0].size(); j++){
+                    resultado.a[i][j] = a[i][j] + matriz.a[i][j];
+                }
+            }
+            return resultado;
+        }    
+};
+int main () {
+   int cases,k;
+   cin >> cases;
+   for(k=0;k<cases;k++) {
+      Matrix x;
+      Matrix y;
+      Matrix result;
+      int n,m,i,j;
+      cin >> n >> m;
+      for(i=0;i<n;i++) {
+         vector<int> b;
+         int num;
+         for(j=0;j<m;j++) {
+            cin >> num;
+            b.push_back(num);
+         }
+         x.a.push_back(b);
+      }
+      for(i=0;i<n;i++) {
+         vector<int> b;
+         int num;
+         for(j=0;j<m;j++) {
+            cin >> num;
+            b.push_back(num);
+         }
+         y.a.push_back(b);
+      }
+      result = x+y;
+      for(i=0;i<n;i++) {
+         for(j=0;j<m;j++) {
+            cout << result.a[i][j] << " ";
+         }
+         cout << endl;
+      }
+   }  
+   return 0;
+}
